c7bbfeb21fefffec5f6468ac29c4d08153d2709b
stephenyup <yupingdeyouxiang@gmail.com>
2026-05-20 10:55:13 +0800

    remove the esp32_prov_blufi and esp32_prov_softap components, use esp32_prov_npm as the unified provision component

--- main/version.h
+++ main/version.h
@@ -3,5 +3,5 @@
 
 #define PROJECT_NAME      "six-iot-sdk"
 #define PROJECT_VERSION   "1.0.0"
-#define VERSION_FULL      "1.0.0_20260416"
-#define BUILD_TIMESTAMP   "20260416"
+#define VERSION_FULL      "1.0.0_20260520"
+#define BUILD_TIMESTAMP   "20260520"
